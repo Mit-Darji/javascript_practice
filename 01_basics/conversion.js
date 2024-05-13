efdac3bddf4eb40db4af149df668286b768ebff9
Mit Darji <107803981+Mit-Darji@users.noreply.github.com>
2024-05-13 08:31:39 +0000

    save

---
+++ 01_basics/conversion.js
@@ -0,0 +1,10 @@
+let age = "33abc"
+
+let valueInNumber = Number(age)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// boolean conversion
+// 1 ==> true ,0 ==> false
+// "" ==> false , "dge" ==> true
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all s code as newer version
+
+// alert( 3 + 3) we are using nodejs not browser
+// number , bigInt , string , boolean , null(standalone value) , object , undefined , symbol (unique)
+
+let age = 3
+console.log(typeof age)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
